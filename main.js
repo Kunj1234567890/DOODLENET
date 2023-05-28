4ee49264bf2e973b6082ac8c4fb254020d56c7ee
Kunj1234567890 <97776880+Kunj1234567890@users.noreply.github.com>
2023-05-28 11:31:18 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,207 @@
+
+
+
+function setup(){
+    canvas = createCanvas(300,400);
+    canvas.center();
+    background('white');
+    canvas.mouseReleased(classifycanvas);
+        synth       =   window.speechSynthesis;
+
+}
+
+function preload (){
+    Classifier = ml5.imageClassifier('DoodleNet');
+   // thickness of pen = 2cm //
+}
+ 
+function draw(){
+    strokeWeight(50);
+    stroke(0);
+    if (mouseIsPressed){
+    line(pmouseX,pmouseY,mouseX,mouseY) ;   }
+}
+function classifycanvas (){
+ Classifier.classify(canvas,gotresults);
+}
+function gotresults(error,results){
+if(error){
+    console.error(error);
+}
+console.log(results);
+document.getElementById("label").innerHTML = 'label :' + results[0].label;
+document.getElementById("confidence").innerHTML = 'confidence : ' + Math.round(results[0].confidence * 100) + '%' ;
+Utterthis = new SpeechSynthesisUtterance(results[0].label);
+synth.speak(Utterthis);               
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+function clearcanvas(){
+    background('white');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
